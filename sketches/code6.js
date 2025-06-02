37efbe334886a9e30072974d26d6e79dc8748b34
Umit Karabulut <umitkarabulut1989@icloud.com>
2025-06-02 16:07:28 +0200

    Exercise 6

--- sketches/code6.js
+++ sketches/code6.js
@@ -8,7 +8,7 @@ function draw_6() {
   // Ceci s’exécute à chaque frame.
   background6(0, 0, 255);
   textSize(30);
-  text("C2", width6 / 2, height6 / 2);
+  text("Pour Umit", width6 / 2, height6 / 2);
 }
 
 // N’oubliez pas d’utiliser la version numérotée des fonctions communes :

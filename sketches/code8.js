39da85b834455d03f859e9260b9a96fe31afa2c6
Umit Karabulut <umitkarabulut1989@icloud.com>
2025-06-02 16:43:31 +0200

    carré mobile

--- sketches/code8.js
+++ sketches/code8.js
@@ -1,4 +1,5 @@
 // Variables globales ici.
+let y = 0; 
 
 function setup_8() {
   // Ceci s’exécute une fois.
@@ -8,7 +9,17 @@ function draw_8() {
   // Ceci s’exécute à chaque frame.
   background8(0, 150, 0);
   textSize(30);
-  text("B3", width8 / 2, height8 / 2);
+  text("Rect", width8 / 2, height8 / 2);
+
+  fill(255);           
+  noStroke();          
+  rect(width8 / 2 - 25, y, 50, 50); 
+
+  y += 2;
+
+  if (y > height8) {
+    y = -100; 
+  }
 }
 
 // N’oubliez pas d’utiliser la version numérotée des fonctions communes :

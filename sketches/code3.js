61c7fcfea40389f5b5a1a2db0f7a9925f46204fa
randypow42 <randy42power@gmail.com>
2025-06-02 16:35:26 +0200

    added color

--- sketches/code3.js
+++ sketches/code3.js
@@ -3,12 +3,16 @@
 function setup_3() {
   // Ceci s’exécute une fois.
 }
+var y = 0
 
 function draw_3() {
   // Ceci s’exécute à chaque frame.
   background3(100, 100, 255);
-  textSize(30);
-  text("salut", width3 / 2, height3 / 2);
+   y = y + 1;
+  rect(100, y, 10, 20)
+  if (y > 200) y = 0
+  //textSize(30);
+  //text("salut", width3 / 2, height3 / 2);
 }
 
 // N’oubliez pas d’utiliser la version numérotée des fonctions communes :

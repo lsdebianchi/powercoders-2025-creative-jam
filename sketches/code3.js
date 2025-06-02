3b5ccee9180752ba680095b1f8b45216a7ea315d
randypow42 <randy42power@gmail.com>
2025-06-02 16:51:16 +0200

    modified movement

--- sketches/code3.js
+++ sketches/code3.js
@@ -3,15 +3,15 @@
 function setup_3() {
   // Ceci s’exécute une fois.
 }
-var y3 = 0;
+var y = 0;
 
 function draw_3() {
   // Ceci s’exécute à chaque frame.
   background3(100, 100, 255);
   fill(255);
-  y3 = y3 + 1;
-  rect(100, y3, 10, 20);
-  if (y3 > 200) y3 = 0;
+  y = y + 1;
+  rect(100, y, 10, 20);
+  if (y > 200) y = 0;
   //textSize(30);
   //text("salut", width3 / 2, height3 / 2);
 }

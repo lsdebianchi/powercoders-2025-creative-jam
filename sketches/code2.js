ffa2a6f2f4ee9c5acfda995a4afbc92274a1af77
TenzinJhopee <tenzin.jeunesse@gmail.com>
2025-06-02 16:46:27 +0200

    circle fallen

--- sketches/code2.js
+++ sketches/code2.js
@@ -1,13 +1,22 @@
-// Variables globales ici.
-function setup_2() {
-  // Ceci s’exécute une fois.
+let x = 0;
+
+function setup() {
+  createCanvas(400, 400);
 }
 
-function draw_2() {
-  // Ceci s’exécute à chaque frame.
-  background2(100, 255, 100);
-  textSize(30);
-  text("Bonjour!!", width2 / 2, height2 / 2);
+function draw() {
+  background(0,0,0);
+  
+  // Move the circle to the right
+  x = x + 2;
+  
+  // Reset position when it goes off screen
+  if (x > width) {
+    x = 0;
+  }
+  
+  // Draw the circle
+  ellipse(200, x, 150, 150);
 }
 
 // N’oubliez pas d’utiliser la version numérotée des fonctions communes :
